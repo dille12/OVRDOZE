eeb4ce385c051ea4e7895f130ca197651334bf60
alturios <paul.kosmala@gmail.com>
2022-05-21 14:56:52 -0700

    changed weapons to do super, and added test stucts

---
+++ test/test_area.py
@@ -0,0 +1,15 @@
+import unittest
+from weapons.area import *
+class test_Grenade(unittest.TestCase):
+    def test_get_string(self):
+        pass
+    def test_molotov_explode(self):
+        pass
+    def test_tick(self):
+        pass
+
+class test_Explosion:
+    def test_damage_actor(self):
+        pass
+    def test_tick(self):
+        pass
---
+++ test/test_turret.py
@@ -0,0 +1,22 @@
+import unittest
+from game_objects.game_object import *
+
+class TestTurret(unittest.TestCase):
+   
+    def test_get_string(self):
+        pass
+    def test_scan_for_enemies(self):
+        pass
+    def test_shoot(self):
+        pass
+    def test_handle_scanning(self):
+        pass
+    def test_draw_bead_on(self):
+        pass
+    def test_draw(self):
+        pass
+    def test_clean_up(self):
+        pass
+    def test_tick(self):
+        pass
+
