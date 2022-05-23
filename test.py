c68a5731c1edc91b81af64e8dc0c0811f7e01033
altruios <49083558+altruios@users.noreply.github.com>
2022-05-23 10:31:51 -0700

    Update test.py

--- test.py
+++ test.py
@@ -4,10 +4,10 @@ cwd = os.getcwd();
 sys.path.append(f'{cwd}/test')
 from test_func import *
 from test_values import *
-from test_server import *
+#from test_server import *
 #broken from test_run import *
 from test_objects import *
-#from test_network import *
+from test_network import *
 from test_network_parser import *
 from test_maps import *
 #from test_map_generator import *
