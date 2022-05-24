495849d539a67721679b919558442ebcaaeef9aa
alturios <paul.kosmala@gmail.com>
2022-05-23 18:39:40 -0700

    await async test

--- test/test_network.py
+++ test/test_network.py
@@ -7,18 +7,19 @@ from _socketEchoServer import socketEchoServerMock
 class TestNetwork(unittest.TestCase):
     @classmethod
     def setUpClass(self):
+        print("cleaning up class")
         self.network=None;
         self.est=None;
     @classmethod
     def tearDownClass(self):
-        print("tear down");
-        print(self.network);
-        print(self.est)
-        print("tear down down")
+        print("tear down\n\n\n");
+        print(self.network,"is network");
+        print(self.est,"is est")
+        print("tear down down\n\n\n")
         self.network=None;
         self.est=None;
         sleep(1)
-    @classmethod
+    
     def startMockEchoServer(self,autoReply=False):
             # echo server..
             es = socketEchoServerMock()
@@ -33,14 +34,14 @@ class TestNetwork(unittest.TestCase):
         assert self.network.client._closed == False
         self.network.client.close()
         assert self.network.client._closed == True
-    def test_send(self):
+    async def test_send(self):
         print('entered: test_send')
         self.startMockEchoServer()
         self.network = Network('127.0.0.1')
         # test send 
         ret = self.network.send("test_str")
         assert ret == "test_str"
-        ret = self.network.send("shutdown_server")
+        ret = await self.network.send("shutdown_server")
         # test send w/ socket _closed
         # expect network.send to return "KILL"
         self.network.client.close()
