d07aff87538344ac09419c5a160585faffab4b57
dille12 <viliam.salli@outlook.com>
2024-04-03 18:02:50 +0300

    Version 0.9.67 - Added a version tracker file

--- GITUPDATE.bat
+++ GITUPDATE.bat
@@ -18,6 +18,11 @@ git add .
 REM Commit the changes with the custom summary and the new number as the message
 git commit -m "Version 0.9.%number% - %custom_summary%"
 
+
+REM Write the commit message to a separate file
+echo Version 0.9.%number% - %custom_summary%>>versiontracker.md
+
+
 git push
 
 REM Pause at the end
